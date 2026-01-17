21d6d8dd92b3f55a18e52f92de66b3e5c968614d
realmzlegend-collab <realmzlegend@gmail.com>
2026-01-17 15:37:57 +0100

    Delete backend/middleware/adminAuth.js

--- backend/middleware/adminAuth.js
+++
@@ -1,9 +0,0 @@
-const adminAuth = (req, res, next) => {
-    // Check if user is admin
-    if (!req.user.isAdmin) {
-        return res.status(403).json({ message: 'Admin access required' });
-    }
-    next();
-};
-
-module.exports = adminAuth;
